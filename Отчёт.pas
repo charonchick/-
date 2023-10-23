16ff9cf42358aa52ece2298ab3eca3c59bb36086
charonchick <146098648+charonchick@users.noreply.github.com>
2023-10-23 20:14:04 +0300

    Add files via upload

---
+++ Отчёт.pas
@@ -0,0 +1,19 @@
+﻿var
+  x, y: real;
+
+begin
+  x := -10;
+  while x <= 3 do
+  begin
+    if x < -8
+      then y := tan(x) / ln(x)
+    else
+    if x < 1
+      then y := ((x * x * x) / 97) - (cos(2 * x) / power(x, 1 / 3))
+    else
+    if x >= 1
+      then y := ((x * x) + power(x, (0.1 * x)));
+    writeln(x:1:1,' = ',y);
+    x := x + 0.1
+  end;
+end.
